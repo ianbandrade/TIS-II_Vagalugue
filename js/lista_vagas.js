2ff1f05461a468aaa409ee1ccbae3dac41780212
Ian Bittencourt <ianbandrade14@gmail.com>
2019-10-26 08:28:26 -0300

    listagem de vagas js css e html

--- js/lista_vagas.js
+++ js/lista_vagas.js
@@ -10,43 +10,31 @@ get();
 async function get() {
   try {
     let response = await $.getJSON("http://127.0.0.1:880/vagas");
+
+    console.log(response);
+
     let vagas = "";
     response.vagas.forEach(element => {
-      vagas += `<div class="ui stackable grid container">
-    <div class="four wide column">
-      <div class="ui card">
-        <div class="ui slide masked reveal image">
-          <img src="imgs/vaga.JPG" class="visible content">
-          <img src="imgs/vaga2.jpg" class="hidden content">
-        </div>
-        <div class="content">
-          <a class="header" >${element.Localizacao.Numero}</a>
-          <div class="meta">
-            <span class="description">Breve descricao</span>
-          </div>
-        </div>
-        <div class="extra content">
-          <a>
-            <i class="table icon"></i>
-            Média
-          </a>
-          <br>
-          <a>
-            <i class="phone icon"></i>
-            (DD) XXXXX-XXXX
-          </a>
-          <br>
-          <a>
-            <i class="dollar sign icon"></i>
-            15,00/dia
-          </a>
+      vagas += `
+      <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img class="card-img" src="${
+          element["Foto"]
+        }"></div>
+        <div class="card_content">
+          <h2 class="card_title">${element["Localizacao"]["Endereco"]}, ${
+        element.Localizacao.Numero
+      }</h2>
+          <p class="card_text">${element["Descricao"]}
+          </p><br><h5>Dimensoes: ${element.Dimensoes.Comprimento}m x ${
+        element.Dimensoes.Altura
+      }m x ${element.Dimensoes.Altura}m </h5>
+          <button class="btn card_btn">Alugar</button>
         </div>
       </div>
-    </div>
-  </div>
-  </div>`;
+    </li>`;
     });
-    document.getElementById("vagas").innerHTML = vagas;
+    document.getElementById("vagas").innerHTML += vagas;
   } catch (errors) {
     console.log(errors);
   }
