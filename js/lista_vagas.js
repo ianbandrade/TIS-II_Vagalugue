980bf2d20b933a11fb0a1e5a4ee2b7ea7490a141
Ian Bittencourt <ianbandrade14@gmail.com>
2019-10-29 22:34:42 -0300

    listagem de vagas - modal

--- js/lista_vagas.js
+++ js/lista_vagas.js
@@ -1,9 +1,33 @@
-$(".special.cards .image").dimmer({
-  on: "hover"
-});
+async function reply_click(clicked_id) {
+  try {
+    console.log(clicked_id);
+    $(() => {
+      $(".ui.test.modal")
+        .modal({
+          closable: false,
+          inverted: true
+        })
+        .modal("attach events", `#${clicked_id}`, "show");
+    });
+  } catch (errors) {
+    console.log(errors);
+  }
+}
 
-var req = new XMLHttpRequest();
-var url = "http://127.0.0.1:880/vagas";
+// A função de pegar o id é essa:
+// function reply_click(clicked_id) {
+//   console.log(clicked_id);
+// }
+
+// A função de dar o "show" no modal:
+// $(() => {
+//   $(".ui.test.modal")
+//     .modal({
+//       closable: false,
+//       inverted: true
+//     })
+//     .modal("attach events", `#${clicked_id}`, "show");
+// });
 
 get();
 
@@ -14,7 +38,7 @@ async function get() {
     console.log(response);
 
     let vagas = "";
-    response.vagas.forEach(element => {
+    response.vagas.forEach((element, index) => {
       vagas += `
       <li class="cards_item">
       <div class="card">
@@ -29,11 +53,44 @@ async function get() {
           </p><br><h5>Dimensoes: ${element.Dimensoes.Comprimento}m x ${
         element.Dimensoes.Altura
       }m x ${element.Dimensoes.Altura}m </h5>
-          <button class="btn card_btn">Alugar</button>
+          <button class="btn card_btn" id="${index}">Alugar</button>
         </div>
       </div>
-    </li>`;
+    </li>
+    <div class="ui test modal" id="">
+    <div class="ui icon header">
+      <i class="user outline icon"></i>
+    </div>
+    <div class="content" class="ui grid">
+
+      <h2>Locador: </h2>
+      <p>Nome: Miguel Rodrigues</p>
+      <p>Indicador da vaga: 402B</p>
+      <h2>Localização: </h2>
+      <p>Rua: Cláudio Manoel, 546</p>
+      <p>Bairro: Savassi</p>
+      <p>Cidade: Belo Horizonte</p>
+      <p>Estado: Minas Gerais</p>
+      <h2>Dimensões:</h2>
+      <p>Altura: 4m</p>
+      <p>Largura: 6m</p>
+      <p>Comprimento: 7m</p>
+      <p class="confirmacao">Confirmar aluguel da vaga?</p>
+
+    </div>
+    <div class="actions">
+      <div class="ui red cancel inverted button">
+        <i class="remove icon"></i>
+        Cancelar
+      </div>
+      <div class="ui green ok inverted button">
+        <i class="checkmark icon"></i>
+        Alugar
+      </div>
+    </div>
+  </div>`;
     });
+
     document.getElementById("vagas").innerHTML += vagas;
   } catch (errors) {
     console.log(errors);
